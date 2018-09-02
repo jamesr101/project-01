1cc7f93b50ab133e7743f09ebe0a6363c5694ed5
James Robins <jamesrobins101@hotmail.com>
2018-09-02 17:34:26 +0100

    fish  move through array of movement patterns and are removed after a timelimit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // ----- GAME SET UP [IMPORTANT: RESET IF BOARD SIZE CHANGES]-----
 const width = 8;
-const numberOfCells = 56;
-const subInitialLocation = 22;
+const numberOfCells = 208;
+const subInitialLocation = 20;
 const initialTime = 60;
 
 // ----- ID AND INDEX SET UP -----
@@ -24,6 +24,7 @@ $(() => {
   const $pointDisplay = $('#points');
   const $restartButton = $('.restartButton');
   const $airTank = $('.airTank');
+  const $cellContainer = $('.cellContainer');
 
   // ----- FISH CONSTRUCTOR -----
   class Fish {
@@ -38,9 +39,13 @@ $(() => {
     move() {
       $cells.eq(this.location).removeClass(this.type);
       this.location += this.movementPatternArray[this.movementPatternIndex];
-      if ((this.location < 0)||(numberOfCells < this.location)){
+      this.movementPatternIndex++;
+      if (this.movementPatternIndex===this.movementPatternArray.length){
+        this.movementPatternIndex = 0;
+      }
+      if ((this.location < 0)||(numberOfCells < this.location)||(this.age<0)){
         this.remove();
-        console.log('Fish swam off');
+        console.log('Fish swam off or died of old age');
       } else {
         $cells.eq(this.location).addClass(this.type);
         this.age--;
@@ -74,12 +79,19 @@ $(() => {
   //----- FISH FUNCTIONS -----
   function spornFish(){
 
-    const greenFish = new Fish(randomLocation(), 'greenFish', 3, [1],0,10);
+    const greenFish = new Fish(randomLocation(), 'greenFish', 3, [1,1,width],0,20);
     $fishInPlay.push(greenFish);
 
-    const redFish = new Fish(randomLocation(), 'redFish', 4, [-1],0,10);
+    const redFish = new Fish(randomLocation(), 'redFish', 4, [-1],0,14);
     $fishInPlay.push(redFish);
 
+    const greenFish2 = new Fish(randomLocation(), 'greenFish', 3, [-1,-1,-width],0,18);
+    $fishInPlay.push(greenFish2);
+
+    const redFish2 = new Fish(randomLocation(), 'redFish', 4, [1],0,16);
+    $fishInPlay.push(redFish2);
+
+    console.log($fishInPlay);
   }
 
 
@@ -177,6 +189,7 @@ $(() => {
   }
 
 
+
   // ----- USER CONTROLS -----
   $(window).on('keydown', keypressed);
   $restartButton.on('click',restartGame);
@@ -192,6 +205,15 @@ $(() => {
       //R key
       restartGame();
     }
+    if (e.keyCode === 81){
+      console.log('Q pressed');
+      //Q key
+      // $cellContainer.scrollTo(0, -500);
+      $cellContainer.animate({
+        scrollTop: $cellContainer.offset().top
+      }, 1000);
+      return false;
+    }
 
     // ----- SUBMARINE CONTROLS -----
     if ((e.keyCode === 39) && ((subLocation+1)%width !== 0)){
@@ -206,6 +228,10 @@ $(() => {
     if (e.keyCode === 40) {
       //DOWN arrow
       e.preventDefault();
+
+      // $cellContainer.scrollTo(0, -500);
+
+      // $cellContainer.scrollTop() ;
       (gameRunning) && (subLocation+width < numberOfCells) && moveSub(width);
     }
     if (e.keyCode === 38) {
